dc78f04c0273bf2f7c749c33444c98ae1e27425b
Mzcg <ZhaochenGu@my.unt.edu>
2023-03-27 11:19:19 -0500

    remove product of two numbers

--- test.py
+++ test.py
@@ -6,7 +6,6 @@ num1 = 3.5
 num2 = 6.4
 
 print("The sum is ", num1 + num2)
-print("The product is ", num1*num2)
 print("The origina number 1 is ", num1, " and the original number 2 is ", num2)
 num = num1
 num1 = num2
