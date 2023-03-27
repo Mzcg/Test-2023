556e62382206e8e46232078adf30a77eeae07012
Mzcg <ZhaochenGu@my.unt.edu>
2023-03-27 10:51:26 -0500

    add number conversion

--- test.py
+++ test.py
@@ -7,4 +7,10 @@ num2 = 6.4
 
 print("The sum is ", num1 + num2)
 print("The product is ", num1*num2)
+print("The origina number 1 is ", num1, " and the original number 2 is ", num2)
+num = num1
+num1 = num2
+num2 = num
+
+print("new num1 is ", num1, " and new num2 is ", num2)
 
